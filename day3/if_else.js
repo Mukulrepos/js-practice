b466cd38156ad263b3c395033256bd27c2537fd0
Mukulrepos <mv282301@gmail.com>
2024-08-07 21:04:01 +0530

    hi

---
+++ day3/if_else.js
@@ -0,0 +1,34 @@
+
+// check number is positive or nto
+// let a =-1;
+
+// if (a >= 0) {
+//   console.log("its is positive number ", a);
+// } else {
+//   console.log("negative number : ",a);
+// }
+
+
+// check the age of person who is elgible  to vote
+let age =21;
+if(age >= 18 ){
+    console.log(":) the person <xy's> is eligible for vote", age);
+    console.log(" :)thank you ");
+}
+else{
+    console.log(" :( sorry ! is not eligible for teh vote ");
+    console.log(": )thank you ");
+    
+    
+}
+
+// // largest in the three number in nested if 
+// let raju_age= 23;
+// let satosh_age = 24;
+// let rupali_age = 15;
+
+// if(raju >= satosh_age){
+//     elseif(raju_age >= rupali_age){
+//         console.log("raju is greater then three persons ",raju_age);
+//     }
+// }
