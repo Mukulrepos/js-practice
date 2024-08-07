e38bf51f3080d1454f6a4c92607e4d5ffeace727
Mukulrepos <mv282301@gmail.com>
2024-08-07 23:01:42 +0530

    all loops in onne file budy

---
+++ day 4/for.js
@@ -0,0 +1,64 @@
+// 1 to 10 counting 
+// for(let i = 1; i<=10;i++){
+//     console.log(i);
+    
+// }
+
+// multiplication table bu number 
+
+// let multi = 5
+// for(let i = 1; i<=10;i++){
+//     console.log(multi+" x "+i+"="+i*multi);
+// }
+// sum of numbers
+// let i = 1 ;
+// let sum = 0 ;
+// let numbers=10;
+// while(i<=numbers){
+//       console.log(i);
+//       sum += i;
+//     i++;
+    
+// }
+// console.log(sum);
+// let i = 10 
+// let numbers = 0 
+// while(i>=numbers){
+//     console.log(i);
+//     i--;
+    
+// }
+// let i = 1
+// do{
+//     console.log(i);
+//     i++;
+    
+// }while(i<=5);
+
+//  right angle pyramid
+// let numbers = 5;
+// for (let i = 0; i < numbers; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     console.log("*");
+//   }
+//   console.log("\n"); // To move to the next line after each row
+// }
+// skiped the numbers
+
+// let num = 5 
+// for(let i = 0 ; i<=10 ;i++){
+//     if(i==num){
+//         continue;
+//     }
+//     console.log(i);
+    
+// }
+
+let num = 7 
+for(let i = 0 ; i<=10 ;i++){
+    if(i==num){
+        break;
+    }
+    console.log(i);
+    
+}
