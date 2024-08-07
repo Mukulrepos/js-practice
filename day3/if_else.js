2772ab995b5f746fd6e61c44aae808ddf1334507
Mukulrepos <mv282301@gmail.com>
2024-08-07 22:24:16 +0530

    only in file

--- day3/if_else.js
+++ day3/if_else.js
@@ -1,4 +1,3 @@
-
 // check number is positive or nto
 // let a =-1;
 
@@ -8,27 +7,71 @@
 //   console.log("negative number : ",a);
 // }
 
-
 // check the age of person who is elgible  to vote
-let age =21;
-if(age >= 18 ){
-    console.log(":) the person <xy's> is eligible for vote", age);
-    console.log(" :)thank you ");
-}
-else{
-    console.log(" :( sorry ! is not eligible for teh vote ");
-    console.log(": )thank you ");
-    
-    
-}
-
-// // largest in the three number in nested if 
-// let raju_age= 23;
-// let satosh_age = 24;
-// let rupali_age = 15;
-
-// if(raju >= satosh_age){
-//     elseif(raju_age >= rupali_age){
-//         console.log("raju is greater then three persons ",raju_age);
-//     }
+// let age = 21;
+// if (age >= 18) {
+//   console.log(":) the person <xy's> is eligible for vote", age);
+//   console.log(" :)thank you ");
+// } else {
+//   console.log(" :( sorry ! is not eligible for teh vote ");
+//   console.log(": )thank you ");
 // }
+
+// switch case day_number
+// let day_numebr = 2;
+
+// switch (day_numebr) {
+//   case 1:
+//     console.log("Mondaye");
+//     break;
+//   case 2:
+//     console.log("Tuesday");
+//     break;
+//   case 3:
+//     console.log("Wednesday");
+//     break;
+//   case 4:
+//     console.log("Thrusday");
+//     break;
+//   case 5:
+//     console.log("Friday");
+//     break;
+//   case 6:
+//     console.log("Saturday");
+//     break;
+//   case 7:
+//     console.log("Sunday");
+//     break;
+//   default:
+//     console.log("sorry i can't understand ");
+// }
+
+// grade .js
+// let score = 85;  // Example score
+
+// switch (true) {
+//     case (score >= 90 && score <= 100):
+//         console.log("A");
+        
+//         break;
+//     case (score >= 80 && score < 90):
+//         console.log("B");
+//         break;
+//     case (score >= 70 && score < 80):
+//         console.log("C");
+//         break;
+//     case (score >= 60 && score < 70):
+//        console.log("D");
+//         break;
+//     case (score >= 0 && score < 60):
+//         console.log("E");
+//         break;
+//     default:
+//         grade = 'INVALID SCORE';
+// }
+
+// Output: B
+
+// let a = 2;
+// let c = (a%2)==0 ? "Even":"Odd";
+// console.log(c);
