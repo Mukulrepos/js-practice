c6d88ec77a1c89d6923577a73c3ee976bccddca1
Mukulrepos <mv282301@gmail.com>
2024-08-07 20:08:21 +0530

    this practice of js

---
+++ day2/arthemetic.js
@@ -0,0 +1,16 @@
+let a = 10;
+let b = 10;
+console.log("add two numbers" + a + b);
+
+let c = 10;
+let d = 10;
+console.log("subtract two number" , c - d);
+let e = 10;
+let f = 10;
+console.log("multiply two numbers " + e * f);
+let g = 10;
+let h = 10;
+console.log("divide two numebrs" + g / h);
+let i = 10;
+let j = 10;
+console.log("modulus two number" + (i % j));
